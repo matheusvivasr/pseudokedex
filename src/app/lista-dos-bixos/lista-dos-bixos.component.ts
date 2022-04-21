e72c521df8b57f61e99a88501fd625b90dd07bd2
matheusvivasr <vivas.matheus@usp.br>
2022-04-21 18:09:49 -0300

    criei o componente 'lista-dos-bixos'

---
+++ src/app/lista-dos-bixos/lista-dos-bixos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-dos-bixos',
+  templateUrl: './lista-dos-bixos.component.html',
+  styleUrls: ['./lista-dos-bixos.component.css']
+})
+export class ListaDosBixosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
