9ccd6669eb7c1e3e83cce19650f5db7bc6db32c5
matheusvivasr <vivas.matheus@usp.br>
2022-04-21 18:13:48 -0300

    component 'ficha-pro-bixo' criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaDosBixosComponent } from './lista-dos-bixos/lista-dos-bixos.component';
+import { FichaProBixoComponent } from './ficha-pro-bixo/ficha-pro-bixo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaDosBixosComponent
+    ListaDosBixosComponent,
+    FichaProBixoComponent
   ],
   imports: [
     BrowserModule,
